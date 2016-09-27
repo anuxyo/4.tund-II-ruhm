4de9ccf0c1a752cc1f94e07faff1c76f8e2db092
anusada <anusada@greeny.cs.tlu.ee>
2016-09-27 15:17:08 +0300

    added a few lines

---
+++ data.php
@@ -0,0 +1,30 @@
+<?php
+
+	require("functions.php");
+	
+	//kui ei ole kasutaja id'd
+	if (!isset ($_SESSION["userId"])) {
+		
+		//suunan sisselogimislehele
+		header("Location: login.php");
+		
+	}
+	
+	//kui on ?logout aadressireal, siis login välja
+	if (isset($_GET["logout"])){
+		
+		session_destroy();
+		header("Location: login.php");
+		
+	}
+
+?>
+<h1>Data</h1>
+
+<p>
+	Tere tulemast<?=$SESSION["userEmail"];?>!
+	<a href="?logout=1">Logi välja</a>
+</p>
+
+
+
---
+++ functions.php
@@ -0,0 +1,121 @@
+<?php
+	//functions
+	//$name="Amu";
+	//var_dump $_GLOBALS($GLOBALS);
+	
+	//see fail, peab olema kıigil lehtedel, kus tahan kasutada SESSION muutujat
+	session_start();
+	
+	//signup
+	
+	function signUp ($email, $password) {
+			
+		$database = "if16_anusada_2";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+	
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
+		
+		echo $mysqli->error;
+		
+		$stmt->bind_param("ss", $email, $password);
+
+		if($stmt->execute()) {
+			echo "salvestamine ınnestus";
+		} else {
+			echo "ERROR".$stmt->error;
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+	
+	
+	function login ($email, $password) {
+		
+		$error = "";
+		
+		$database = "if16_anusada_2";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+	
+		$stmt = $mysqli->prepare("SELECT id, email, password, created FROM user_sample WHERE email = ?");
+		
+		echo $mysqli->error;
+		
+		//asendan k¸sim‰rgi
+		$stmt->bind_param("s", $email);
+		
+		//m‰‰ran v‰‰rtused muutujatesse
+		$stmt->bind_result($id, $emailFromDb, $passwordFromDb, $created);
+		
+		$stmt->execute();
+		
+		//andmed tulid andmebaasist vıi mitte
+		//on tıene kui on v‰hemalt ¸ks vaste
+		if($stmt->fetch()){
+			
+			//oli sellise meiliga kasutaja
+			//password millega kasutaja tahab sisse logida
+			$hash = hash("sha512", $password);
+			if ($hash == $passwordFromDb) {
+				echo  "Kasutaja logis sisse".$id;
+				
+				//m‰‰ran sessioni muutujad, millele saan ligi teistelt lehtedelt
+				$_SESSION["userId"] = $id;
+				$_SESSION["userEmail"] = $emailFromDb;
+				
+				header("Location: data.php");
+				
+			} else {
+				$error = "vale parool";
+			}
+			
+		} else {
+			
+			//ei leidnud kasutajat sellise meiliga
+			$error = "ei ole sellist emaili";
+		}
+		
+		return $error;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//function sum($x, $y) {
+		
+		//return $x + $y;
+		
+	//}
+
+	//echo sum(845983, 1232432);
+	//echo "<br>";
+	//echo sum(1,1);
+
+	//function hello($firstname, $lastname) {
+		
+		//return "Tere tulemast ".$firstname." ".$lastname."!";
+		
+	//}
+	//echo sum(845983, 1232432);
+	//echo "<br>"
+	//echo hello("Anu", "Sadam");
+	
+	
+	
+?>
